f0fbf1b6b098f9d0a6837d7f766ce21de7ca2b99
Mehedi Hasan <thisismehedihasan0.1@gmail.com>
2025-07-03 19:31:36 +0600

    feat: check two linked list size is same or not

---
+++ semester-02/basic-data-structures/module-7.5-week-02-practice-day-02/pp1-check-size.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+  int val;
+  Node *next;
+
+  Node(int val)
+  {
+    this->val = val;
+    this->next = NULL;
+  }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+  Node *new_node = new Node(val);
+
+  if (head)
+  {
+    tail->next = new_node;
+    tail = new_node;
+  }
+  else
+  {
+    head = new_node;
+    tail = new_node;
+  }
+}
+
+void take_input(Node *&head, Node *&tail)
+{
+  while (1)
+  {
+    int val;
+    cin >> val;
+
+    if (val == -1)
+    {
+      break;
+    }
+
+    insert_at_tail(head, tail, val);
+  }
+}
+
+int sz(Node *head)
+{
+  int cnt = 0;
+
+  while (head)
+  {
+    cnt++;
+    head = head->next;
+  }
+
+  return cnt;
+}
+
+int main()
+{
+  Node *head1 = NULL, *head2 = NULL, *tail1 = NULL, *tail2 = NULL;
+
+  take_input(head1, tail1);
+  take_input(head2, tail2);
+
+  int size1 = sz(head1), size2 = sz(head2);
+
+  size1 == size2 ? cout << "YES" : cout << "NO";
+
+  return 0;
+}
