1c995f010e530b226c4bf1556fe307467eb77ca1
Mehedi Hasan <thisismehedihasan0.1@gmail.com>
2025-12-18 23:22:05 +0600

    fix: folder name change

---
+++ contestes/codechef/starters/217-div-4/b.cpp
@@ -0,0 +1,64 @@
+/* ---------------------------------------------------------
+Hi there, I'm here.
+Btw, I'm Mehedi Hasan from Bangladesh. Let's get connected.
+LinkedIn & CP: zenmh
+--------------------------------------------------------- */
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define nl '\n'
+#define ll long long
+#define ld long double
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define zenmh ios::sync_with_stdio(false), cin.tie(nullptr)
+#define ordered_set tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+/*
+- find_by_order(k): iterator to k-th element (0-indexed)
+- order_of_key(x): count of elements strictly less than x
+*/
+
+void run()
+{
+  int n;
+  cin >> n;
+
+  int a[n];
+
+  ll sum = 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+    sum += a[i];
+  }
+
+  if (sum < 0)
+  {
+    int x = ceil(abs(sum) / (n * 1.0));
+    cout << x << nl;
+    return;
+  }
+
+  cout << 0 << nl;
+}
+
+int main()
+{
+  zenmh;
+
+  int tc;
+  cin >> tc;
+
+  while (tc--)
+  {
+    run();
+  }
+
+  return 0;
+}
---
+++ xpsc/vjudge/week-09-random-problem-solving-1/a.buying-new-tablet.cpp
@@ -0,0 +1,60 @@
+/* ---------------------------------------------------------
+Hi there, I'm here.
+Btw, I'm Mehedi Hasan from Bangladesh. Let's get connected.
+LinkedIn & CP: zenmh
+--------------------------------------------------------- */
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define nl '\n'
+#define ll long long
+#define ld long double
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define zenmh ios::sync_with_stdio(false), cin.tie(nullptr)
+#define ordered_set tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+/*
+- find_by_order(k): iterator to k-th element (0-indexed)
+- order_of_key(x): count of elements strictly less than x
+*/
+
+void run()
+{
+  ll n, b;
+  cin >> n >> b;
+
+  ll ans = LLONG_MIN;
+
+  while (n--)
+  {
+    ll w, h, p;
+    cin >> w >> h >> p;
+
+    if (p <= b)
+    {
+      ans = max(ans, w * h);
+    }
+  }
+
+  ans == LLONG_MIN ? cout << "no tablet\n" : cout << ans << nl;
+}
+
+int main()
+{
+  zenmh;
+
+  int tc;
+  cin >> tc;
+
+  while (tc--)
+  {
+    run();
+  }
+
+  return 0;
+}
